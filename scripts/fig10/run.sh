c1c2d078a36ef8913d1f3f5d5e07a803cff16949
Jiaqi Lou <jiaqil6@illinois.edu>
2025-04-17 13:50:17 -0500

    update client run scripts

--- scripts/fig10/run.sh
+++ scripts/fig10/run.sh
@@ -2,6 +2,10 @@
 
 # set -e
 
+# Source environment variables from env_setup.sh
+source ../common/env_setup.sh
+echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
 python3 run_dpdk-pd.py
 python3 run_baseline.py
 python3 run_dlb.py
--- scripts/fig9/run.sh
+++ scripts/fig9/run.sh
@@ -2,5 +2,9 @@
 
 # set -e
 
+# Source environment variables from env_setup.sh
+source ../common/env_setup.sh
+echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
 python3 run_acc.py
 python3 run_cpu.py
